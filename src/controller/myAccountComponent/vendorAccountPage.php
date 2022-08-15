b084cb6b640a1f7d66dd13dd60faf6ec5807d388
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-08-15 20:02:14 +0700

    Slight change on vendor's page

--- src/controller/myAccountComponent/vendorAccountPage.php
+++ src/controller/myAccountComponent/vendorAccountPage.php
@@ -15,6 +15,7 @@ class VendorAccount {
                     <img class="product-img" src="assets/image/product/<?php echo $key;?>.jpg" alt="<?php echo $key;?>"r>
                     <div class="vendor-product-info">
                         <p class="vendor-product-name"><?php echo $value["name"]?></p>
+                        <p>ID: <?php echo $key?></p>
                         <p class="vendor-product-price"><?php echo number_format($value["price"])?> VND</p>
                         <p class="vendor-product-category">Category: <?php echo $value["category"]?></p>
                         <p class="vendor-product-sold">Sold: <?php echo $value["sold"]?></p>
