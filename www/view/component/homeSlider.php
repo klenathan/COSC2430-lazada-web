6e99118a3effcb620464e889c8d8730a66ee5f98
Dong <phamvodong0811@gmail.com>
2022-08-20 02:16:28 +0700

    modified header and category drop down

--- www/view/component/homeSlider.php
+++ www/view/component/homeSlider.php
@@ -1,3 +1,3 @@
 <div class="carousel">
-    <img src="https://media.giphy.com/media/iGM5sy0YJ19Yj3X6Gr/giphy.gif" alt="test-product-img">
+    <img src="../../assets/image/backgroundLogin.jpg" alt="test-product-img">
 </div>
