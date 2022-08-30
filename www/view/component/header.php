78d564c7cc8768525b8cc6e3ec0e9f6a5d10af25
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-08-30 14:58:37 +0700

    improved vendor page fix & removed header
    improved vendor page fix & removed header search bar for non-customer

--- www/view/component/header.php
+++ www/view/component/header.php
@@ -22,13 +22,22 @@
         </a>
     </div>
 
-    <div class="header-mid">
-        <form action="search" method="get">
-            <input name="search" type="text" id="search" 
-            autocomplete="off" placeholder="Search product... ">
-            <input id="submit-btn" type="submit" value="">
-        </form>
-    </div>
+    <?php
+    if (isset($_SESSION["user_detail"])){
+        if (json_decode($_SESSION["user_detail"], true)["accountType"] == "customer") {
+            ?>
+        <div class="header-mid">
+                <form action="search" method="get">
+                    <input name="search" type="text" id="search" 
+                    autocomplete="off" placeholder="Search product... ">
+                    <input id="submit-btn" type="submit" value="">
+                </form>
+            </div>
+            <?php
+        }
+    }
+    
+    ?>
 
     <div class="header-right">
             <?php
